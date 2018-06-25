36054ffffb7b5078982c762cd7496e1eee3dbdb9
AndreasDerGaul <40361405+AndreasDerGaul@users.noreply.github.com>
2018-06-25 15:08:04 +0200

    Add files via upload
    Tische/Treppe hinzugefügt, Tische mit 4 Hockern verbunden

--- taverne.cs
+++ taverne.cs
@@ -15,7 +15,8 @@ public class taverne : MonoBehaviour {
 
     //Deklaration der Objekte in den Bereichen
     GameObject buehne;
-    GameObject tisch;
+    GameObject tisch1;
+    GameObject tisch2;
     GameObject bar;
     GameObject stuhl;
     GameObject fass;
@@ -25,6 +26,15 @@ public class taverne : MonoBehaviour {
     GameObject hocker4;
     GameObject hocker5;
     GameObject hocker6;
+    GameObject hocker7;
+    GameObject hocker8;
+    GameObject hocker9;
+    GameObject hocker10;
+    GameObject hocker11;
+    GameObject hocker12;
+    GameObject hocker13;
+    GameObject hocker14;
+    GameObject treppe;
 
     //Deklaration der Meshes und Hilfslisten für Bereiche
     Mesh barbermesh;
@@ -52,8 +62,133 @@ public class taverne : MonoBehaviour {
     List<Vector3> buVert;
     List<int> buTri;
 
-    //hockermethode
-    void hockermesh(float xpos, float ypos, float zpos, GameObject hocker) {
+    Mesh timesh;
+    List<Vector3> tiVert;
+    List<int> tiTri;
+
+    Mesh tremesh;
+    List<Vector3> treVert;
+    List<int> treTri;
+
+    //waagerechte tischmethode
+    void tischmeshwaage(float xpos, float ypos, float zpos, GameObject tisch, GameObject hocker1, GameObject hocker2, GameObject hocker3, GameObject hocker4) {
+
+        tiVert = new List<Vector3>();
+        tiTri = new List<int>();
+
+        tiVert.Add(new Vector3(xpos + 0.8f, ypos, zpos - 0.4f));
+        tiVert.Add(new Vector3(xpos + 0.8f, ypos + 0.6f, zpos - 0.4f));
+        tiVert.Add(new Vector3(xpos + 0.8f, ypos, zpos + 0.4f));
+        tiVert.Add(new Vector3(xpos + 0.8f, ypos + 0.6f, zpos + 0.4f));
+
+        tiVert.Add(new Vector3(xpos - 0.8f, ypos, zpos + 0.4f));
+        tiVert.Add(new Vector3(xpos - 0.8f, ypos + 0.6f, zpos + 0.4f));
+
+        tiVert.Add(new Vector3(xpos - 0.8f, ypos, zpos - 0.4f));
+        tiVert.Add(new Vector3(xpos - 0.8f, ypos + 0.6f, zpos - 0.4f));
+
+        tiVert.Add(new Vector3(xpos + 0.8f, ypos, zpos - 0.4f));
+        tiVert.Add(new Vector3(xpos + 0.8f, ypos + 0.6f, zpos - 0.4f));
+
+        tiVert.Add(new Vector3(xpos - 0.8f, ypos + 0.6f, zpos - 0.4f));
+        tiVert.Add(new Vector3(xpos - 0.8f, ypos + 0.6f, zpos + 0.4f));
+        tiVert.Add(new Vector3(xpos + 0.8f, ypos + 0.6f, zpos - 0.4f));
+        tiVert.Add(new Vector3(xpos + 0.8f, ypos + 0.6f, zpos + 0.4f));
+
+        timesh = new Mesh();
+        timesh.vertices = tiVert.ToArray();
+
+        tisch.GetComponent<MeshFilter>().mesh = timesh;
+
+        for (int i = 0; i < timesh.vertices.Length - 2; i++)
+        {
+            if (i % 2 == 0)
+            {
+                tiTri.Add(i);
+                tiTri.Add(i + 1);
+                tiTri.Add(i + 2);
+            }
+
+            if (i % 2 == 1)
+            {
+                tiTri.Add(i + 1);
+                tiTri.Add(i);
+                tiTri.Add(i + 2);
+            }
+        }
+
+        timesh.triangles = tiTri.ToArray();
+
+        tisch.GetComponent<Renderer>().material.color = new Color(0.0f,0.0f,1.0f);
+
+        //Hocker werden an Tisch angesetzt...Austausch mit Stuehlen?
+        hockermesh(xpos - 0.4f, ypos, zpos + 0.8f, hocker1);
+        hockermesh(xpos + 0.4f, ypos, zpos + 0.8f, hocker2);
+        hockermesh(xpos - 0.4f, ypos, zpos - 0.8f, hocker3);
+        hockermesh(xpos + 0.4f, ypos, zpos - 0.8f, hocker4);
+    }
+
+    //senkrechte Tischmethode
+    void tischmeshsenk(float xpos, float ypos, float zpos, GameObject tisch, GameObject hocker1, GameObject hocker2, GameObject hocker3, GameObject hocker4)
+    {
+
+        tiVert = new List<Vector3>();
+        tiTri = new List<int>();
+
+        tiVert.Add(new Vector3(xpos + 0.4f, ypos, zpos - 0.8f));
+        tiVert.Add(new Vector3(xpos + 0.4f, ypos + 0.6f, zpos - 0.8f));
+        tiVert.Add(new Vector3(xpos + 0.4f, ypos, zpos + 0.8f));
+        tiVert.Add(new Vector3(xpos + 0.4f, ypos + 0.6f, zpos + 0.8f));
+
+        tiVert.Add(new Vector3(xpos - 0.4f, ypos, zpos + 0.8f));
+        tiVert.Add(new Vector3(xpos - 0.4f, ypos + 0.6f, zpos + 0.8f));
+
+        tiVert.Add(new Vector3(xpos - 0.4f, ypos, zpos - 0.8f));
+        tiVert.Add(new Vector3(xpos - 0.4f, ypos + 0.6f, zpos - 0.8f));
+
+        tiVert.Add(new Vector3(xpos + 0.4f, ypos, zpos - 0.8f));
+        tiVert.Add(new Vector3(xpos + 0.4f, ypos + 0.6f, zpos - 0.8f));
+
+        tiVert.Add(new Vector3(xpos - 0.4f, ypos + 0.6f, zpos - 0.8f));
+        tiVert.Add(new Vector3(xpos - 0.4f, ypos + 0.6f, zpos + 0.8f));
+        tiVert.Add(new Vector3(xpos + 0.4f, ypos + 0.6f, zpos - 0.8f));
+        tiVert.Add(new Vector3(xpos + 0.4f, ypos + 0.6f, zpos + 0.8f));
+
+        timesh = new Mesh();
+        timesh.vertices = tiVert.ToArray();
+
+        tisch.GetComponent<MeshFilter>().mesh = timesh;
+
+        for (int i = 0; i < timesh.vertices.Length - 2; i++)
+        {
+            if (i % 2 == 0)
+            {
+                tiTri.Add(i);
+                tiTri.Add(i + 1);
+                tiTri.Add(i + 2);
+            }
+
+            if (i % 2 == 1)
+            {
+                tiTri.Add(i + 1);
+                tiTri.Add(i);
+                tiTri.Add(i + 2);
+            }
+        }
+
+        timesh.triangles = tiTri.ToArray();
+
+        tisch.GetComponent<Renderer>().material.color = new Color(0.0f, 0.0f, 1.0f);
+
+        //Hocker werden an Tisch angesetzt...Austausch mit Stuehlen?
+        hockermesh(xpos - 0.8f, ypos, zpos + 0.4f, hocker1);
+        hockermesh(xpos + 0.8f, ypos, zpos + 0.4f, hocker2);
+        hockermesh(xpos - 0.8f, ypos, zpos - 0.4f, hocker3);
+        hockermesh(xpos + 0.8f, ypos, zpos - 0.4f, hocker4);
+    }
+
+    void hockermesh(float xpos, float ypos, float zpos, GameObject hocker)
+    {
 
         hoVert = new List<Vector3>();
         hoTri = new List<int>();
@@ -101,10 +236,81 @@ public class taverne : MonoBehaviour {
 
         homesh.triangles = hoTri.ToArray();
 
-        hocker.GetComponent<Renderer>().material.color = new Color(0.0f,1.0f,1.0f);
+        hocker.GetComponent<Renderer>().material.color = new Color(0.0f, 1.0f, 1.0f);
     }
 
-void Start () {
+    //Treppe an der Buehne
+    void treppenmesh(float xpos, float ypos, float zpos, GameObject treppe) {
+        treVert = new List<Vector3>();
+        treTri = new List<int>();
+
+        treVert.Add(new Vector3(xpos - 0.4f, 0.1f, zpos - 0.5f));
+        treVert.Add(new Vector3(xpos - 0.4f, 0.1f + (0.5f/3), zpos - 0.5f));
+        treVert.Add(new Vector3(xpos, 0.1f, zpos - 0.5f));
+        treVert.Add(new Vector3(xpos, 0.1f + (0.5f/3), zpos - 0.5f));
+
+        treVert.Add(new Vector3(xpos, 0.1f, zpos + 0.5f));
+        treVert.Add(new Vector3(xpos, 0.1f + (0.5f/3), zpos + 0.5f));
+
+        treVert.Add(new Vector3(xpos - 0.4f, 0.1f, zpos + 0.5f));
+        treVert.Add(new Vector3(xpos - 0.4f, 0.1f + (0.5f/3), zpos + 0.5f));
+
+        treVert.Add(new Vector3(xpos - 0.4f, 0.1f, zpos - 0.5f));
+        treVert.Add(new Vector3(xpos - 0.4f, 0.1f + (0.5f / 3), zpos - 0.5f));
+        //hier muss cnt erhöht werden sonst zu viele tris
+        treVert.Add(new Vector3(xpos - 0.4f, 0.1f + (0.5f / 3), zpos - 0.5f));
+        treVert.Add(new Vector3(xpos - 0.4f, 0.1f + (0.5f / 3), zpos + 0.5f));
+        treVert.Add(new Vector3(xpos - 0.2f, 0.1f + (0.5f / 3), zpos - 0.5f));
+        treVert.Add(new Vector3(xpos - 0.2f, 0.1f + (0.5f / 3), zpos + 0.5f));
+        //cnt++
+        treVert.Add(new Vector3(xpos - 0.2f, 0.1f + (0.5f / 3), zpos - 0.5f));
+        treVert.Add(new Vector3(xpos - 0.2f, 0.1f + ((0.5f / 3)*2), zpos - 0.5f));
+        treVert.Add(new Vector3(xpos, 0.1f + (0.5f / 3), zpos - 0.5f));
+        treVert.Add(new Vector3(xpos, 0.1f + ((0.5f / 3) * 2), zpos - 0.5f));
+
+        treVert.Add(new Vector3(xpos, 0.1f + (0.5f / 3), zpos + 0.5f));
+        treVert.Add(new Vector3(xpos, 0.1f + ((0.5f / 3) * 2), zpos + 0.5f));
+
+        treVert.Add(new Vector3(xpos - 0.2f, 0.1f + (0.5f / 3), zpos + 0.5f));
+        treVert.Add(new Vector3(xpos - 0.2f, 0.1f + ((0.5f / 3) * 2), zpos + 0.5f));
+
+        treVert.Add(new Vector3(xpos - 0.2f, 0.1f + (0.5f / 3), zpos + 0.5f));
+        treVert.Add(new Vector3(xpos - 0.2f, 0.1f + ((0.5f / 3) * 2), zpos + 0.5f));
+        //cnt++
+        treVert.Add(new Vector3(xpos - 0.2f, 0.1f + ((0.5f / 3) * 2), zpos - 0.5f));
+        treVert.Add(new Vector3(xpos - 0.2f, 0.1f + ((0.5f / 3) * 2), zpos + 0.5f));
+        treVert.Add(new Vector3(xpos, 0.1f + ((0.5f / 3) * 2), zpos - 0.5f));
+        treVert.Add(new Vector3(xpos, 0.1f + ((0.5f / 3) * 2), zpos + 0.5f));
+
+
+        tremesh = new Mesh();
+        tremesh.vertices = treVert.ToArray();
+
+        treppe.GetComponent<MeshFilter>().mesh =tremesh;
+
+        for (int i = 0; i < tremesh.vertices.Length - 2; i++)
+        {
+            if (i % 2 == 0)
+            {
+                treTri.Add(i);
+                treTri.Add(i + 1);
+                treTri.Add(i + 2);
+            }
+
+            if (i % 2 == 1)
+            {
+                treTri.Add(i + 1);
+                treTri.Add(i);
+                treTri.Add(i + 2);
+            }
+        }
+
+        tremesh.triangles = treTri.ToArray();
+
+        treppe.GetComponent<Renderer>().material.color = new Color(0.0f, 1.0f, 1.0f);
+    }
+
+    void Start () {
 
         //Instanzierung der Bereiche auf der Plane Boden
         boden = GameObject.CreatePrimitive(PrimitiveType.Plane);
@@ -121,6 +327,17 @@ void Start () {
         hocker4 = GameObject.CreatePrimitive(PrimitiveType.Quad);
         hocker5 = GameObject.CreatePrimitive(PrimitiveType.Quad);
         hocker6 = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        hocker7 = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        hocker8 = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        hocker9 = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        hocker10 = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        hocker11 = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        hocker12 = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        hocker13 = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        hocker14 = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        tisch1 = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        tisch2 = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        treppe = GameObject.CreatePrimitive(PrimitiveType.Quad);
 
         //Instanzierung der Hilfslisten der Bereiche
         barberVert = new List<Vector3>();
@@ -136,6 +353,30 @@ void Start () {
 
         buVert = new List<Vector3>();
         buTri = new List<int>();
+
+        //Name der Objekte/Bereiche
+        boden.name = "Boden";
+        barbereich.name = "Barbereich";
+        buehnenbereich.name = "Buehnenbereich";
+        tischbereich.name = "Tischbereich";
+        buehne.name = "Buehne";
+        bar.name = "Bar";
+        tisch1.name = "Tisch1";
+        tisch2.name = "Tisch2";
+        hocker1.name = "Hocker1";
+        hocker2.name = "Hocker2";
+        hocker3.name = "Hocker3";
+        hocker4.name = "Hocker4";
+        hocker5.name = "Hocker5";
+        hocker6.name = "Hocker6";
+        hocker7.name = "Hocker7";
+        hocker8.name = "Hocker8";
+        hocker9.name = "Hocker9";
+        hocker10.name = "Hocker10";
+        hocker11.name = "Hocker11";
+        hocker12.name = "Hocker12";
+        hocker13.name = "Hocker13";
+        hocker14.name = "Hocker14";
 
         //Bereiche der Taverne einteilen
 
@@ -210,7 +451,6 @@ void Start () {
         //Objekte verschieben/skalieren/rotieren
 
         //buehne
-
 
         buVert.Add(new Vector3( -2.5f, 0.1f, 2.5f));
         buVert.Add(new Vector3( -2.5f, 0.6f, 2.5f));
@@ -345,12 +585,17 @@ void Start () {
         hockermesh(-2.4f, 0.1f, -1.3f, hocker5);
         hockermesh(-2.4f, 0.1f, -0.5f, hocker6);
 
+        //Tische
+        tischmeshwaage(0.5f, 0.1f, -1, tisch1, hocker7, hocker8, hocker9, hocker10);
+        tischmeshsenk(2.5f, 0.1f, -2.5f, tisch2, hocker11, hocker12, hocker13, hocker14);
+
         //Farben der Objekte
         buehne.GetComponent<Renderer>().material.color = new Color(1.0f, 0.0f, 1.0f);
         bar.GetComponent<Renderer>().material.color = new Color(1.0f, 0.0f, 1.0f);
         boden.GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f);
 
-        Debug.Log(bumesh.triangles.Length);
+        //treppe
+        treppenmesh(-2.5f, 0.6f, 3.75f, treppe);
     }
 
     // Update is called once per frame
