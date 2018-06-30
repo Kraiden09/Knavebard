d755a20f12f45f2365b6718f47ddfb0caf9f47f3
Sven Schickentanz <40361432+Kraiden09@users.noreply.github.com>
2018-06-30 15:35:49 +0200

    Initialisierung
    Für die Charaktererstellung

---
+++ Init.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init : MonoBehaviour {
+    public bool initialized = false;
+
+	// Use this for initialization
+	void Start () {
+        GameObject bard = (GameObject)Instantiate(Resources.Load("Prefab/Character"));
+        bard.name = "Bard";
+        initialized = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
