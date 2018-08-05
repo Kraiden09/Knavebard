30e33aa5810ca51fd96e962aab1564864463c1f0
JorgDerGrosse <40361380+JorgDerGrosse@users.noreply.github.com>
2018-08-05 19:30:36 +0200

    Add files via upload
    Grundlegendes Movement einer Kamera innheralb einer Grenze
    Arbeitszeit: ca. 25 min

---
+++ CameraControl.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/* simpelste Bewegung der Kamera innerhalb simpler Grenzen
+ * bisher: oben, unten, links, rechts, zoom, zoomout
+ * 
+ */
+public class CameraControl : MonoBehaviour {
+    //Camera cam;
+
+
+    //Bewegungsgeschwindigkeit
+    public float camSpeed;
+
+    float borderx, bordery, borderz;
+
+	// Use this for initialization
+	void Start () {
+        //  cam = Camera.main;
+        //indirekte Grenzen durch urspruengliche Kameraposition
+        borderx = transform.position.x;
+        bordery = transform.position.y;
+        borderz = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //w-a-s-d
+        if (Input.GetKey(KeyCode.W))
+        {
+            //Grenze
+            if (transform.position.y - bordery < 1)
+            {
+                transform.Translate(new Vector3(0, camSpeed * Time.deltaTime, 0));
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (transform.position.x - borderx > -1)
+            {
+                transform.Translate(new Vector3(-camSpeed * Time.deltaTime, 0, 0));
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (transform.position.y - bordery > -1)
+            {
+                transform.Translate(new Vector3(0, -camSpeed * Time.deltaTime, 0));
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            if (transform.position.x - borderx < 1)
+            {
+                transform.Translate(new Vector3(camSpeed * Time.deltaTime, 0, 0));
+            }
+        }
+
+        //Zoom + Zoom out
+        if (Input.GetKey(KeyCode.PageUp))
+        {
+            if (transform.position.z - borderz < 1)
+            {
+                transform.Translate(new Vector3(0, 0, camSpeed * Time.deltaTime));
+            }
+        }
+
+        if (Input.GetKey(KeyCode.PageDown))
+        {
+            if (transform.position.z - borderz > -1)
+            {
+                transform.Translate(new Vector3(0, 0, -camSpeed * Time.deltaTime));
+            }
+        }
+
+
+    }
+}
