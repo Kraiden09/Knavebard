101c52b83872af2e11c2fb7d2902ac682c618eb5
Sven Schickentanz <40361432+Kraiden09@users.noreply.github.com>
2018-08-15 22:30:09 +0200

    Add files via upload
    Getter hinzugefügt

--- Init.cs
+++ Init.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Init : MonoBehaviour {
+    GameObject bard;
     public bool initialized = false;
 
 	// Use this for initialization
 	void Start () {
-        GameObject bard = (GameObject)Instantiate(Resources.Load("Prefab/Character"));
+        bard = (GameObject)Instantiate(Resources.Load("Prefab/Character"));
         bard.name = "Bard";
         initialized = true;
     }
@@ -16,4 +17,8 @@ public class Init : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public GameObject getBard() {
+        return bard;
+    }
 }
