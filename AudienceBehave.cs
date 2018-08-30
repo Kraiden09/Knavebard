8fe82fadfd07a480e84326dccc5091c80e3e5246
JorgDerGrosse <40361380+JorgDerGrosse@users.noreply.github.com>
2018-08-30 13:02:06 +0200

    erste, simple Formel für die Stimmung
    Stimmung (Jam) wird kontinuierlich gemessen und ausgegeben

--- AudienceBehave.cs
+++ AudienceBehave.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 public class AudienceBehave : MonoBehaviour {
 
     GameObject[] crowd;
+    //getting the grades
+    NoteBoard NoteBoard;
 
     //quantification of the mood
-    int jam;
+    public int jam;
+    //Grades given by NoteBoard
+    int great, good, bad;
 
 	// Use this for initialization
 	void Start () {
+        great = 0;
+        good = 0;
+        bad = 0;
+
         //measure jam after each beat
-        measureJam();
-        waitBeat();
-	}
+        StartCoroutine(WaitForNoteBoard());
+        StartCoroutine(WaitBeat());
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-
-        
-        
-	}
+    }
 
     //returns a percentage of good/bad-mooded audiencemembers
     public GameObject[] getAudiencePart(int percentage)
@@ -32,32 +38,42 @@ public class AudienceBehave : MonoBehaviour {
     }
 
     //jumping with jam-meter
-    public void rave()
+    public void Rave()
     {
 
     }
 
 
     //formula for jam-meter
-    public void measureJam()
+    public void MeasureJam()
     {
-        // jam =
-        
+        bad = NoteBoard.bad;
+        good = NoteBoard.good;
+        great = NoteBoard.great;
+        jam = (great * 2 + good) - bad * 4;
+        print("Jam =" + jam);
     }
 
     //continuous measuremeant after each beat
-    IEnumerator waitBeat()
+    IEnumerator  WaitBeat()
+    {
+        print("JAMMMMM");
+        
+        yield return new WaitForSeconds(0.49180327868f);
+        MeasureJam();
+        StartCoroutine(WaitBeat());
+    }
+
+    IEnumerator WaitForNoteBoard()
     {
         yield return new WaitForSeconds(0.49180327868f);
-        measureJam();
-        waitBeat();
+        NoteBoard = GameObject.Find("NoteBoard").GetComponent<NoteBoard>();
     }
 
 
     // moving by little hops (be as cute as possible)
-    public void hopTo(GameObject destination)
+    public void HopTo(GameObject destination)
     {
-     
-    }
 
+    }
 }
