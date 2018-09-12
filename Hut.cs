ac5123ba025945abcaa709533307f6d73b333a22
Schwabi93 <40383170+Schwabi93@users.noreply.github.com>
2018-09-13 00:37:00 +0200

    Add files via upload

---
+++ Hut.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hut : MonoBehaviour {
+    GameObject LightHut, HutZylinder;
+    private AudienceBehave jam;
+
+    public int jamSchwabi;
+
+    // Use this for initialization
+    void Start () {
+        jam = FindObjectOfType<AudienceBehave>();
+
+        LightHut = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        HutZylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+
+        LightHut.name = "LightHut";
+        LightHut.transform.localScale = new Vector3(1, 0.01f, 1);
+        LightHut.transform.Translate(0, 0, 0);
+        LightHut.GetComponent<Renderer>().material.color = new Color(0.5f, 0.5f, 0.5f);
+
+        HutZylinder.name = "HutZylinder";
+        HutZylinder.transform.localScale = new Vector3(0.8f, 0.15f, 0.8f);
+        HutZylinder.transform.Translate(0, 0.15f, 0);
+        HutZylinder.GetComponent<Renderer>().material.color = new Color(0.5f, 0.5f, 0.5f);
+
+        HutZylinder.transform.parent = LightHut.transform;
+
+        LightHut.transform.Translate(6.45f, 0, 3.8f);
+
+        Debug.Log(jam.jam);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (jam.jam < -15) {
+            jamSchwabi = 0;
+        }
+        else if (jam.jam >= -15 && jam.jam < 15) {
+            jamSchwabi = 1;
+        }
+        else {
+            jamSchwabi = 2;
+        }
+
+        switch (jamSchwabi) {
+            case 0:
+                //Debug.Log("CASE=rot");
+                LightHut.GetComponent<Renderer>().material.color = new Color(1, 0, 0);
+                HutZylinder.GetComponent<Renderer>().material.color = new Color(1, 0, 0);
+                break;
+            case 1:
+                //Debug.Log("CASE=gelb");
+                LightHut.GetComponent<Renderer>().material.color = new Color(1, 1, 0);
+                HutZylinder.GetComponent<Renderer>().material.color = new Color(1, 1, 0);
+                break;
+            case 2:
+                //Debug.Log("CASE=grün");
+                LightHut.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
+                HutZylinder.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
+                break;
+            default:
+                //Debug.Log("Lichtwechsel - Fehler");
+                break;
+        }
+    }
+}
