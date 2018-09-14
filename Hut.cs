35ca11da9c367676b58510bdae0c523a58aa2ec0
Schwabi93 <40383170+Schwabi93@users.noreply.github.com>
2018-09-14 14:44:48 +0200

    Add files via upload

--- Hut.cs
+++ Hut.cs
@@ -6,10 +6,19 @@ public class Hut : MonoBehaviour {
     GameObject LightHut, HutZylinder;
     private AudienceBehave jam;
 
+    private int tmp = 0;
+    private Color red;
+    private Color yellow;
+    private Color green;
+
     public int jamSchwabi;
 
     // Use this for initialization
     void Start () {
+        red = new Color(1, 0, 0, 1);
+        yellow = new Color(1, 1, 0, 1);
+        green = new Color(0, 1, 0, 1);
+
         jam = FindObjectOfType<AudienceBehave>();
 
         LightHut = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
@@ -18,12 +27,12 @@ public class Hut : MonoBehaviour {
         LightHut.name = "LightHut";
         LightHut.transform.localScale = new Vector3(1, 0.01f, 1);
         LightHut.transform.Translate(0, 0, 0);
-        LightHut.GetComponent<Renderer>().material.color = new Color(0.5f, 0.5f, 0.5f);
+        LightHut.GetComponent<Renderer>().material.color = new Color(1f, 1f, 0f);
 
         HutZylinder.name = "HutZylinder";
         HutZylinder.transform.localScale = new Vector3(0.8f, 0.15f, 0.8f);
         HutZylinder.transform.Translate(0, 0.15f, 0);
-        HutZylinder.GetComponent<Renderer>().material.color = new Color(0.5f, 0.5f, 0.5f);
+        HutZylinder.GetComponent<Renderer>().material.color = new Color(1f, 1f, 0f);
 
         HutZylinder.transform.parent = LightHut.transform;
 
@@ -39,6 +48,7 @@ public class Hut : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //Debug.Log(LightHut.GetComponent<Renderer>().material.color);
         if (jam.jam < -20) {
             jamSchwabi = 0;
         }
@@ -52,22 +62,74 @@ public class Hut : MonoBehaviour {
         switch (jamSchwabi) {
             case 0:
                 //Debug.Log("CASE=rot");
-                LightHut.GetComponent<Renderer>().material.color = new Color(1, 0, 0);
-                HutZylinder.GetComponent<Renderer>().material.color = new Color(1, 0, 0);
+                StartFadeHut(red);
+                //LightHut.GetComponent<Renderer>().material.color = new Color(1, 0, 0);
+                //HutZylinder.GetComponent<Renderer>().material.color = new Color(1, 0, 0);
                 break;
             case 1:
                 //Debug.Log("CASE=gelb");
-                LightHut.GetComponent<Renderer>().material.color = new Color(1, 1, 0);
-                HutZylinder.GetComponent<Renderer>().material.color = new Color(1, 1, 0);
+                StartFadeHut(yellow);
+                //LightHut.GetComponent<Renderer>().material.color = new Color(1, 1, 0);
+                //HutZylinder.GetComponent<Renderer>().material.color = new Color(1, 1, 0);
                 break;
             case 2:
                 //Debug.Log("CASE=grün");
-                LightHut.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
-                HutZylinder.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
+                StartFadeHut(green);
+                //LightHut.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
+                //HutZylinder.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
                 break;
             default:
                 //Debug.Log("Lichtwechsel - Fehler");
                 break;
         }
+    }
+    void StartFadeHut(Color Color) {
+        StartCoroutine(FadeHutCore(Color));
+    }
+
+    IEnumerator FadeHutCore(Color Color) {
+        if (tmp == 1) {
+            tmp = 0;
+            StopCoroutine(FadeHutCore(Color));
+        }
+        tmp = 1;
+        while (Color != LightHut.GetComponent<Renderer>().material.color) {
+            if (Color == red) {
+                if (LightHut.GetComponent<Renderer>().material.color.r < 1) {
+                    LightHut.GetComponent<Renderer>().material.color = LightHut.GetComponent<Renderer>().material.color + new Color(0.02f, 0, 0, 0);
+                    HutZylinder.GetComponent<Renderer>().material.color = HutZylinder.GetComponent<Renderer>().material.color + new Color(0.02f, 0, 0, 0);
+                }
+                if (LightHut.GetComponent<Renderer>().material.color.g > 0) {
+                    LightHut.GetComponent<Renderer>().material.color = LightHut.GetComponent<Renderer>().material.color + new Color(0, -0.02f, 0, 0);
+                    HutZylinder.GetComponent<Renderer>().material.color = HutZylinder.GetComponent<Renderer>().material.color + new Color(0, -0.02f, 0, 0);
+                }
+                //Debug.Log("RICHTUNG=rot");
+            }
+            else if (Color == yellow) {
+                if (LightHut.GetComponent<Renderer>().material.color.r < 1) {
+                    LightHut.GetComponent<Renderer>().material.color = LightHut.GetComponent<Renderer>().material.color + new Color(0.02f, 0, 0, 0);
+                    HutZylinder.GetComponent<Renderer>().material.color = HutZylinder.GetComponent<Renderer>().material.color + new Color(0.02f, 0, 0, 0);
+                }
+                if (LightHut.GetComponent<Renderer>().material.color.g < 1) {
+                    LightHut.GetComponent<Renderer>().material.color = LightHut.GetComponent<Renderer>().material.color + new Color(0, 0.02f, 0, 0);
+                    HutZylinder.GetComponent<Renderer>().material.color = HutZylinder.GetComponent<Renderer>().material.color + new Color(0, 0.02f, 0, 0);
+                }
+                //Debug.Log("RICHTUNG=gelb");
+            }
+            else if (Color == green) {
+                if (LightHut.GetComponent<Renderer>().material.color.r > 0) {
+                    LightHut.GetComponent<Renderer>().material.color = LightHut.GetComponent<Renderer>().material.color + new Color(-0.02f, 0, 0, 0);
+                    HutZylinder.GetComponent<Renderer>().material.color = HutZylinder.GetComponent<Renderer>().material.color + new Color(-0.02f, 0, 0, 0);
+                }
+                if (LightHut.GetComponent<Renderer>().material.color.g < 1) {
+                    LightHut.GetComponent<Renderer>().material.color = LightHut.GetComponent<Renderer>().material.color + new Color(0, 0.02f, 0, 0);
+                    HutZylinder.GetComponent<Renderer>().material.color = HutZylinder.GetComponent<Renderer>().material.color + new Color(0, 0.02f, 0, 0);
+                }
+                //Debug.Log("RICHTUNG=grün");
+            }
+            yield return new WaitForSeconds(1f);
+        }
+        tmp = 0;
+        yield return null;
     }
 }
