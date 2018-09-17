70200bc69959fa48a7ae9a85dffaec80aba56bc1
Schwabi93 <40383170+Schwabi93@users.noreply.github.com>
2018-09-17 15:15:37 +0200

    Add files via upload

--- HutTriggerScript.cs
+++ HutTriggerScript.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HutTriggerScript : MonoBehaviour {
     private GameObject Hut;
     private GameObject Bard;
 
+    public Text text;
+    GameObject interaction;
+
     // Use this for initialization
     void Start() {
+        interaction = GameObject.Find("InteractionText");
+        text = interaction.GetComponent<Text>();
+
         Hut = GameObject.Find("LightHut");
         Bard = GameObject.Find("Bard");
     }
@@ -19,7 +26,7 @@ public class HutTriggerScript : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.name == "Bard") {
-
+            text.text = "Press H to wear hat";
         }
     }
 
@@ -30,13 +37,15 @@ public class HutTriggerScript : MonoBehaviour {
                 Hut.transform.position = Bard.transform.position + new Vector3(0, 0.4f, 0);
                 Hut.transform.parent = Bard.transform;
                 Debug.Log("Läuft bei mir!");
+                Destroy(this);
+                text.text = "";
             }
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if (other.gameObject.name == "Bard") {
-
+            text.text = "";
         }
     }
 }
